f549955ca3320926f4245628f4e9d8635cb572cc
BryanDomino <66131719+BryanDomino@users.noreply.github.com>
2023-07-18 10:18:02 +0100

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [ "$EXPECTED_BRANCH" -ne "$CURRENT_BRANCH" ]
-do
+then
   git checkout EXPECTED_BRANCH
 fi
 R -e 'rmarkdown::run("src/app/app.Rmd", shiny_args = list(port = 8888, host="0.0.0.0"))'
